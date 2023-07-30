af34c29d3311520cb54aeb5fb9076ea9d5a0bae8
George Mathew <geopm@outlook.com>
2023-07-30 15:22:46 +0100

    test

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+    import "../app.css";
+</script>
+
+<nav>
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+    <a href="/contact">Contact</a>
+</nav>
+
+<slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,14 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<title>Hello</title>
+
+<div class="index">
+    <h1>Welcome to SvelteKit</h1>
+    <p>
+        Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+    </p>
+    <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum voluptas
+        dolorem nihil vel assumenda quod aperiam ipsum totam magnam rerum
+        accusamus odio, sed similique quos perferendis deleniti laboriosam ex
+        non?
+    </p>
+</div>
